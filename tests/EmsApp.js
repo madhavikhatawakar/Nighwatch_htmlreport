68cbe5a1a567d59698146bc742fd5f6ea9467e6d
madhavikhatawakar <madhavikh@cybage.com>
2015-10-30 17:21:47 +0530

    google test added

--- tests/EmsApp.js
+++ tests/EmsApp.js
@@ -1,4 +1,4 @@
-module.exports = {
+/*module.exports = {
   'Demo test Ems App' : function (client) {
     client
       .url('http://172.27.59.112:5024/EmployeeApplicationSprint4-1.0-SNAPSHOT')
@@ -13,8 +13,8 @@ module.exports = {
   }
     
    
-};
-/*module.exports = {
+};*/
+module.exports = {
   'Demo test Google' : function (client) {
     client
       .url('http://www.google.com')
@@ -29,5 +29,5 @@ module.exports = {
         'Rembrandt - Wikipedia')
       .end();
   }
-};*/
+};
 
